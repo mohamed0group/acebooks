f3d3221b90531bdfaf52e60fcb496b0015da8653
Abdalla Mahmoud <the94air@gmail.com>
2018-04-17 20:55:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import App from 'layouts/Home/Home.jsx';
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-    <Router history={hist}>
+    <HashRouter history={hist}>
         <Switch>
             <App/>
             {/* {
@@ -32,5 +32,5 @@ ReactDOM.render(
                 })
             } */}
         </Switch>
-    </Router>
+    </HashRouter>
 , document.getElementById('root'));
