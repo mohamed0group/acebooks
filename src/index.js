6128a1311f96e541687417bc099168ef72915aa1
Mohamed Abdeen Mohamed Ahmed <34784505+mohamed0group@users.noreply.github.com>
2018-04-23 05:12:04 +0000

    setting the static router

--- src/index.js
+++ src/index.js
@@ -2,35 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import {
-    HashRouter,
-    Route,
-    Switch
+    BrowserRouter,
+    StaticRouter
 } from 'react-router-dom';
-
+import ReactDomServer ftom 'react-dom/server';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/scss/now-ui-dashboard.css';
 import 'assets/css/demo.css';
 import App from 'layouts/Home/Home.jsx';
 
 // import indexRoutes from 'routes/index.jsx';
+createServer((req, res) => {
+  const context = {}
 
+  const html = ReactDOMServer.renderToString(
+    <StaticRouter
+      location={req.url}
+      context={context}
+    >
+      <App/>
+    </StaticRouter>
+  )
+
+  if (context.url) {
+    res.writeHead(301, {
+      Location: context.url
+    })
+    res.end()
+  } else {
+    res.write(`
+      <!doctype html>
+      <div id="root">${html}</div>
+    `)
+    res.end()
+  }
+}).listen(3000)
 const hist = createBrowserHistory();
-
-ReactDOM.render(
-    <HashRouter history={hist}>
-        <Switch>
-            <App/>
-            {/* {
-                indexRoutes.map((prop,key) => {
-                    return (
-                        <Route
-                            path={prop.path}
-                            key={key}
-                            component={prop.component}
-                        />
-                    );
-                })
-            } */}
-        </Switch>
-    </HashRouter>
-, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter history = {hist}>
+    <App/>
+  </BrowserRouter>
+), document.getElementById('root'));
